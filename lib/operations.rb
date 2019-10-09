09327f5916772856ffbe07f39c6447923e14ea07
Justin Phillips <justinphil02@yahoo.com>
2019-10-09 03:11:43 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,7 +11,7 @@ end
 
 def not_safe?(speed)
 safe = true 
-speed < 40 ? safe = true   : safe = false   
+speed < 40 ? safe = false    : safe = true    
 speed > 60 ? safe = false   : safe = true   
 return !safe 
 	
